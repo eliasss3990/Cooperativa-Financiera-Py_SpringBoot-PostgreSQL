ee86fa72ff1997e87ad56d40b9c7a26a12bc589c
Elias Gonzalez <elias.gonzalez@ncoders.solutions>
2026-03-14 17:51:35 -0300

    renombrar clase principal y la de test

--- src/main/java/developers/CoopFinancieraApplication.java
+++ src/main/java/developers/CoopFinancieraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendParaProyectoCooperativoFinancieroParaguayoApplication {
+public class CoopFinancieraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendParaProyectoCooperativoFinancieroParaguayoApplication.class, args);
+		SpringApplication.run(CoopFinancieraApplication.class, args);
 	}
 
 }
